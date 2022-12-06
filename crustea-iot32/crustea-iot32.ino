abe607f124d738a18567f56f6fb344576737247a
laodeshaldanfalih <77shaldan77@gmail.com>
2022-12-06 23:59:02 +0700

    adding dummy sensor data and mqtt connection

--- crustea-iot32/crustea-iot32.ino
+++ crustea-iot32/crustea-iot32.ino
@@ -9,15 +9,12 @@
 
 #endif
 
-//#if !defined(PZEM_SERIAL)
-//#define PZEM_SERIAL Serial2
-//#endif
 
 /* Hardware Serial2 is only available on certain boards.
  * For example the Arduino MEGA 2560
 */
 #if defined(USE_SOFTWARE_SERIAL)
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 /*************************
  *  Use SoftwareSerial for communication
  * ---------------------
@@ -25,8 +22,8 @@
  * The ESP32 platform does not support the SoftwareSerial as of now 
  * Here we initialize the PZEM on SoftwareSerial with RX/TX pins PZEM_RX_PIN and PZEM_TX_PIN
  */
-SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
-PZEM004Tv30 pzem(pzemSWSerial);
+//SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+//PZEM004Tv30 pzem(pzemSWSerial);
 
 #elif defined(ESP32)
 /*************************
@@ -63,6 +60,18 @@ int relayPin = 23; //set pin 8 for relay output
 
 #define WIFI_SSID "ipongggggg"
 #define WIFI_PASSWORD "12345678"
+
+// thingsboard
+
+#include <ThingsBoard.h>
+#define TOKEN "EBII_ACCESS_TOKEN"
+#define thingsboardServer "thingsboard.cloud"
+WiFiClient wifiClient;
+
+ThingsBoard tb(wifiClient);
+
+int status = WL_IDLE_STATUS;
+unsigned long lastSend;
 
 void pzemRead(){
 
@@ -134,6 +143,64 @@ void powerSystem(int relayMode){
 
 }
 
+void getAndSendData(){
+   Serial.println("Collecting temperature data.");
+   
+  float salinity = random(1,100);
+  float temperature = random(1,100);
+  float pH = random(1,100);
+  float DO = random(1,100);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(salinity) || isnan(temperature) || isnan(pH) || isnan(DO)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println("Sending data to ThingsBoard:");
+  Serial.print("Salinity: ");
+  Serial.print(salinity);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" *C ");
+  Serial.print("pH: ");
+  Serial.print(pH);
+  Serial.print(" %\t");
+  Serial.print("DO: ");
+  Serial.print(DO);
+
+  tb.sendTelemetryFloat("temperature", temperature);
+  tb.sendTelemetryFloat("salinity", salinity);
+  tb.sendTelemetryFloat("DO", DO);
+  tb.sendTelemetryFloat("pH", pH);
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!tb.connected()) {
+    status = WiFi.status();
+    if ( status != WL_CONNECTED) {
+      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+      Serial.println("Connected to AP");
+    }
+    Serial.print("Connecting to ThingsBoard node ...");
+    if ( tb.connect(thingsboardServer, TOKEN) ) {
+      Serial.println( "[DONE]" );
+    } else {
+      Serial.print( "[FAILED]" );
+      Serial.println( " : retrying in 5 seconds]" );
+      // Wait 5 seconds before retrying
+      delay( 5000 );
+    }
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
@@ -147,15 +214,30 @@ void setup() {
   Serial.print("Connected with IP: ");
   Serial.println(WiFi.localIP());
   Serial.println();
+
+  lastSend = 0;
   
   pinMode(relayPin, OUTPUT);
 
 }
 
 void loop() {
+
+  if ( !tb.connected() ) {
+    reconnect();
+  }
+
+  if ( millis() - lastSend > 1000 ) { // Update and send only after 1 seconds
+    getAndSendData();
+    lastSend = millis();
+  }
+
+  tb.loop();
   
   // memantau sensor PZEM
   pzemMonitor();
+
+  
   
 
 }
